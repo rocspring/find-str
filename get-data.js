b82d8eb5ff495caf511c62d7874737e6716213f3
wangshaopeng <wsp000000@163.com>
2016-04-26 11:51:10 +0800

    完善统计的Excel表格

--- get-data.js
+++ get-data.js
@@ -9,7 +9,8 @@ var strObj = {
     "http": {
         name:'http',
         strArr: ['BNJS.http.get', 'BNJS.http.post', 'BNJS.http.sign', 'BNJS.http.getNA', 'BNJS.http.postNA', 'BNJS.http.getCatgData'],
-        numObjArr:[]
+        numObjArr:[],
+        allNum: 0
     },
     "location": {
         name:'location',
@@ -17,27 +18,31 @@ var strObj = {
         'BNJS.location.cityCode', 'BNJS.location.cityName', 'BNJS.location.shortCityName', 'BNJS.location.cityUrl', 'BNJS.location.latitude',
         'BNJS.location.longitude','BNJS.location.address', 'BNJS.location.districtId', 'BNJS.location.districtName', 'BNJS.location.hasLocation',
         'BNJS.location.selectCityCode','BNJS.location.selectCityName', 'BNJS.location.selectShortCityName', 'BNJS.location.selectCityUrl'],
-        numObjArr:[]
+        numObjArr:[],
+        allNum: 0
     },
     "device": {
         name:'device',
         strArr: ['BNJS.device.setScreenRotate', 'BNJS.device.call', 'BNJS.device.getTelBook', 'BNJS.device.name',
     'BNJS.device.platform', 'BNJS.device.os', 'BNJS.device.screenWidth', 'BNJS.device.screenHeight'],
-        numObjArr:[]
+        numObjArr:[],
+        allNum: 0
     },
     "evn": {
         name:'evn',
         strArr: ['BNJS.env.network', 'BNJS.env.getConfig', 'BNJS.env.acCode', 'BNJS.env.getCompConfig', 'BNJS.env.cuid', 'BNJS.env.appName',
         'BNJS.env.appVersion', 'BNJS.env.appChannel', 'BNJS.env.sidList', 'BNJS.env.uuid', 'BNJS.env.packageName', 'BNJS.env.compVersion', 'BNJS.env.compId',
         'BNJS.env.compPage'],
-        numObjArr:[]
+        numObjArr:[],
+        allNum: 0
     },
     "account": {
         name:'account',
         strArr: ['BNJS.account.login', 'BNJS.account.getMobile', 'BNJS.account.getSecretAccount', 'BNJS.account.getCommonSecretAccount',
         'BNJS.account.getDpassWithMobile', 'BNJS.account.refreshCaptchaImage', 'BNJS.account.loginWithMobile', 'BNJS.account.isLogin',
         'BNJS.account.uid', 'BNJS.account.uName', 'BNJS.account.displayName', 'BNJS.account.bduss'],
-        numObjArr:[]
+        numObjArr:[],
+        allNum: 0
     },
     "page": {
         name:'page',
@@ -45,7 +50,8 @@ var strObj = {
         'BNJS.page.unRegisterReceiver', 'BNJS.page.sendBroadcast', 'BNJS.page.startBind', 'BNJS.page.onBtnBackClick', 'BNJS.page.orderConfirm',
         'BNJS.page.reShow', 'BNJS.page.enableBounce', 'BNJS.page.onCityChange', 'BNJS.page.onMessage', 'BNJS.page.postMessage', 'BNJS.page.startPageforResult',
         'BNJS.page.getAlbum', 'BNJS.page.leave', 'BNJS.page.startCreditPay'],
-        numObjArr:[]
+        numObjArr:[],
+        allNum: 0
     },
     "ui": {
         name:'ui',
@@ -55,42 +61,50 @@ var strObj = {
         'BNJS.ui.share', 'BNJS.ui.showLoadingPage', 'BNJS.ui.hideLoadingPage', 'BNJS.ui.showErrorPage', 'BNJS.ui.hideErrorPage',
         'BNJS.ui.nativeInterfere', 'BNJS.ui.closePullAction', 'BNJS.ui.hasBanner', 'BNJS.ui.copyText', 'BNJS.ui.toggleBtnBack',
         'BNJS.ui.actionSheet.show'],
-        numObjArr:[]
+        numObjArr:[],
+        allNum: 0
     },
     "hardware": {
         name:'hardware',
         strArr: ['BNJS.hardware.scanQRCode', 'BNJS.hardware.getWifiAround'],
-        numObjArr:[]
+        numObjArr:[],
+        allNum: 0
     },
     "statistic": {
         name:'statistic',
         strArr: ['BNJS.statistic.addLog', 'BNJS.statistic.addCtag', 'BNJS.statistic.addMTJ'],
-        numObjArr:[]
+        numObjArr:[],
+        allNum: 0
     },
     "localStorage": {
         name:'localStorage',
         strArr: ['BNJS.localStorage.setItem', 'BNJS.localStorage.getItem', 'BNJS.localStorage.removeItem'],
-        numObjArr:[]
+        numObjArr:[],
+        allNum: 0
     },
     "baseService": {
         name:'baseService',
         strArr: ['BNJS.baseService'],
-        numObjArr:[]
+        numObjArr:[],
+        allNum: 0
     },
     "BNJSReady": {
         name:'BNJSReady',
         strArr: ['BNJSReady'],
-        numObjArr:[]
+        numObjArr:[],
+        allNum: 0
     },
     "enableDebug": {
         name:'enableDebug',
         strArr: ['BNJS.enableDebug'],
-        numObjArr:[]
+        numObjArr:[],
+        allNum: 0
     },
     "version": {
         name:'version',
         strArr: ['BNJS.version'],
-        numObjArr:[]
+        numObjArr:[],
+        allNum: 0
     }
 
 };
